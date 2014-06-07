2696a93114864b5665a86b8172b4f7a1a908ad87
julman <johnulman@gmail.com>
2014-06-07 14:44:20 -0400

    Update blue.py

--- blue.py
+++ blue.py
@@ -1,6 +1,6 @@
 import bluetooth
 import time
-result = bluetooth.lookup_name('', timeout=5)
+result = bluetooth.lookup_name('10:D5:42:BB:48:5F', timeout=5)
 if(result != None):
 	print "im here!"
 else:
