f561ddb8b1b6faec53f2d4756ce33e86cdd434cc
julman <johnulman@gmail.com>
2014-06-19 20:25:10 +0000

    remove test files

--- test.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree as ET
-tree = ET.parse('macs.xml')
-root = tree.getroot()
-bashcommand = "mplayer"
-import subprocess
-for child in root:
-  print child.find('name').text
-  child.find('mac').text
