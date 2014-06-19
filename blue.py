dfa675bf952c688429ec45c272f8c7f1e020b429
julman <johnulman@gmail.com>
2014-06-19 17:17:12 +0000

    Add bash command to play music

--- blue.py
+++ blue.py
@@ -4,9 +4,14 @@ import time
 import xml.etree.ElementTree as ET
 tree = ET.parse('macs.xml')
 root = tree.getroot()
+bashcommand = "mplayer"
+import subprocess
 for child in root:
 		result = bluetooth.lookup_name(child.find("mac").text, timeout=5)
 		if(result != None):
-			print child.find("name").text + " is here!"
+			print child.find("name").text + (" is here!")
+			bashcommand = "mplayer " + child.find("mp3").text
+			process = subprocess.Popen(bashcommand.split(), stdout=subprocess.PIPE)
+			output = process.communicate()[0]
 		else:
 			print child.find("name").text + " is not here :("
