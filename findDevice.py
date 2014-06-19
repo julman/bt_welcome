cb2a28417a1081ee044c903d2fa4186f5921b5a2
julman <johnulman@gmail.com>
2014-06-19 16:05:04 -0400

    test add device

--- findDevice.py
+++ findDevice.py
@@ -1,14 +1,45 @@
 import bluetooth
 
-adress = None
+def get_name()
+	name = input('Enter your name: ')
+	print('Hello', name)
+	return name
+
+def get_song()
+	song = input('Enter your name: ')
+	print('the song you chose is:', song)
+	return song
+
+def put_in_xml(name, song, mac)
+	tree = ET.parse('macs.xml')
+	root = tree.getroot()
+	newActorNode = ET.Element('actor')
+	newName = ET.Element('name')
+	newName.text = name
+	newMac = ET.Element('mac')
+	newMac.text = mac
+	newModDT = ET.Element('modDT')
+	newModDT.text = ''
+	newMp3 = ET.Element('mp3')
+	newMp3.text = song
+	newActorNode.append(newName)
+	newActorNode.append(newMac)
+	newActorNode.append(newModDT)
+	newActorNode.append(newMp3)
+	root.append(newActorNode)
+	tree.write('macs.xml')
+	print "you done!"
 
 devices = bluetooth.discover_devices()
 if(len(devices)>1):
-	print "only one device at a time bitches!"
+	print "Only one device at a time, itches!"
 else:
 	if(len(devices)<1):
-		print "need at least one devices to be discoerable!"
+		print "need at least one devices to be discoverable!"
 	else:
-	
+
 		for discovered_address in devices:
 			print "found " + discovered_address
+			name = get_name()
+			song = get_song()
+			put_in_xml(name, song, discovered_address)
